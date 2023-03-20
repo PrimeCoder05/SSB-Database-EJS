440a5d3ae4d5832970e0b8f91ddee8ca94dc8eb5
PrimeCoder05 <lukas.a.pena@gmail.com>
2023-03-20 16:11:07 -0400

    working render controller and properly nested code

--- routes/viewRouters/viewRouter.js
+++ routes/viewRouters/viewRouter.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+	renderAllCharacters
+} = require("../../controllers/view/viewController");
+
 router.get("/", (req, res) => {
 	res.render("index");
 });
+
+router.get("/allChars", renderAllCharacters);
 
 module.exports = router;
